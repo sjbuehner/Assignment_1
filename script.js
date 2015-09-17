d2deb4c54dc4e5456eb719d65ec8f2abf1f8f7b8
sjbuehner <sandi@sandiconsulting.com>
2015-09-16 17:58:23 -0700

    Adding index.html and script.js to Assignment_1

---
+++ script.js
@@ -0,0 +1,164 @@
+//Step one
+//var someMonth;
+//function currentMonth;
+//const currentMonth;
+/*var month = new Array();
+	month[0] = "June";
+	month[1] = "July";
+	month[2] = "August"; 
+	month[3] = "September";*/
+	
+//function myLibrary;
+
+//Step two
+// 5
+//"Sandi"
+// true
+// nul
+
+//Step three
+//var x = 12;
+//var x++;
+//var x = 20;
+//var y = 5;
+//var x * y;
+
+//Step four
+//var firstName;
+//var lastName;
+//var strAddress;
+//var city;
+//var state;
+//var strzipCode;
+//var yourAge;
+//var referralSource;
+//var bln mayWeContactYou;
+
+//Step five
+//var firstName = "Sandi";
+//var strZipCode = "92115";
+//var yourAge = 20;
+
+//Step six
+/*var firstName = "Sidney";
+var birthYear = 1990;
+console.log(firstName + birthYear);*/
+
+/*var test =(6 === "six");
+var name = "Sandi";
+console.log (test + "Sandi");*/
+
+/*var test = (6 >= 5);
+var x = 27;
+console.log(x + test);*/
+
+
+//Step seven
+/*var firstName = "Mark";
+console.log(firstName + " is a " + describe + " guy.");
+var describe = "nice";*/
+
+//Step eight
+/*var someString = "Who once said, \"Only two things are infinite, the universe and the human stupidity, and I'm not sure about the former.\""; 
+console.log(someString);*/
+
+//Step nine
+/* var x = 22;
+var x = null;
+console.log(x);*/
+
+/*var cat;
+console.log(cat);*/
+
+//Step ten
+/*var name = "Duane";
+console.log(typeof name);*/
+
+/*var x = 143;
+console.log(typeof x);*/
+
+/*var x = (67 == 67);
+console.log(typeof x);*/
+
+/*var x = new Object();
+console.log(typeof x);*/
+
+/*var mapLocation;
+console.log(typeof mapLocation);*/
+
+//Step eleven
+//alert("Hello" + " " +"Sandi Buehner" + ", " + "welcome to the JavaScript class!");
+
+//Step twelve
+/*var name = "Sandi";
+alert("Hello" + " " + name + ", " + "welcome to the JavaScript class!");*/
+
+//Step thirteen
+/*var name = "Sandi";
+var course = "JavaScript";
+alert("Hello" + " " + name + ", " + "welcome to the" + " " + course + " " + "class!");*/
+
+//Step fourteen
+/*var name = "Sandi";
+var course = "JavaScript";
+alert("Hello" + " " + name + ".\n" + "Welcome to the" + " " + course + " " + "class!");*/
+
+//Step fifteen
+/*var name = prompt("What is your name?");
+var course = "Javascript";
+alert("Hello" + " " + name + ".\n" + "Welcome to the" + " " + course + " " + "class!");*/
+
+//Step sixteen
+/*var name = prompt("What is your name?");
+var course = prompt("What class are you taking?");
+alert("Hello" + " " + name + ".\n" + "Welcome to the" + " " + course + " " + "class!");*/
+
+//Step seventeen
+/*var x = 10;
+var y = 20;
+console.log(x + y);*/
+
+//Step eighteen
+/*var x = 20;
+var x = x + 20;
+console.log(x);*/
+
+//Step nineteen
+/*var x = 20;
+var x = x * 5;
+console.log(x);*/
+
+//Step twenty
+/*var x = 20 % 3;
+var x = x/1;
+console.log(x);*/
+
+//Step twenty-one
+/*var x = 10;
+var y = 5; 
+if (x >= y && x==10){
+	console.log(x - y)
+};*/
+
+
+//Step twenty-two
+/*var a = 21;
+var c = 7;
+if (a == 21 && c > 7){
+	console.log(a/c)
+}
+else (console.log("Can not be computed."));*/
+
+//Step twenty-three
+/*var x = new Object("widgets");
+console.log(x);*/
+
+//Step twenty-four
+/*var noteBook = new Object();
+console.log(noteBook instanceof Object);*/
+
+//Step twenty-five
+/*var noteBook = new Object();
+console.log(noteBook instanceof Number);*/
+
+
